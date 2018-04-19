d99468055560c30f568cfcb38be595e218081a3c
guobei <957488094@qq.com>
2018-04-19 17:45:28 +0800

    第一次提交

---
+++ Demo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ${GuoBei}
+ * Date: 2018/4/1
+ * Time: 14:25
+ */
+
+namespace Demo;
+
+
+class Demo
+{
+    public function __construct()
+    {
+        echo __CLASS__;
+    }
+}
