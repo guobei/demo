2a06ca51494b3adb734be06fc6dbce8a57dc5949
guobei <957488094@qq.com>
2018-04-24 20:41:05 +0800

    change

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 //PHP生成器
 <?php
-    function
+    function makeRange($length)
+    {
+        for ($i = 0 ; $i< $length ; $i++){
+            yield $i;
+        }
+    }
+
+    foreach (makeRange(1000000) as $i){
+        echo $i,PHP_EOL;
+    }
